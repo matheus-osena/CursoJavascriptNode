6f87bfb1cc22ab1ca1262c7c4f347c77f6f596b5
Matheus Sena <matheus_osena@hotmail.com>
2023-03-03 14:09:24 -0300

    Teste

---
+++ aula 1/mod1.js
@@ -0,0 +1,11 @@
+const nome = 'Luiz';
+const sobrenome = 'Miranda';
+
+const falaNome = () => {
+    console.log(nome,sobrenome);
+};
+
+exports.nome = nome;
+exports.sobrenome = sobrenome;
+exports.falaNome = falaNome;
+
