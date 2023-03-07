9141e9e4567282d62cf2bdc3b5e41d0150568014
Matheus Sena <matheus_osena@hotmail.com>
2023-03-07 17:10:46 -0300

    mais aulas

---
+++ aula9/controllers/homeController.js
@@ -0,0 +1,7 @@
+exports.paginaInicial = (req, res) => {
+    res.send('<form action = "/" method="POST"> Nome do cliente : <input type="text" name="qualquer coisa"><br> Outro campo: <input type="text" name="aquioutrocampo"><button>Olá mundo</button></form>')
+}
+
+exports.trataPost = (req,res) => {
+    res.send('Ei, sou sua nova rota de Post.')
+}
