0d9515945847afd12e871af97fd0f262be596076
Matheus Sena <matheus_osena@hotmail.com>
2023-03-07 18:06:09 -0300

    aula11

--- aula10/server.js
+++ aula10/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes.js');
+const path = require('path');
 
 app.use(express.urlencoded({extended: true}));
+
+
+app.set('views', path.resolve(__dirname,'src','views'));
+app.set('view engine','ejs');
+
 app.use(routes);
 
 app.listen(3000, ()=> {
